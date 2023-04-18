89227829eab100007dc1bb1101f927d7c27b362a
aismlv <adilzhan.ismailov@gmail.com>
2023-04-18 23:57:15 +0100

    Refactor

--- minivnn/minivnn.py
+++ minivnn/minivnn.py
@@ -1,40 +1,52 @@
 import numpy as np
+from typing import List, Tuple
+
+
+def normalize(embedding: np.ndarray) -> np.ndarray:
+    return embedding / np.linalg.norm(embedding)
 
 
 class Index:
-    def __init__(self, dim: int, metric="dot"):
+    def __init__(self, dim: int, metric: str = "dot") -> None:
+        self.dim = dim
         self.metric = metric
         self.embeddings = np.empty((0, dim))
-        self.index_map = []
+        self.index_map: List[int] = []
 
-    def add(self, index, embedding):
+    def add(self, index: int, embedding: np.ndarray) -> None:
         if index in self.index_map:
-            raise ValueError(f"Index {index} already exists")
+            raise ValueError(f"Index {index} already exists.")
+
+        embedding = embedding.reshape(1, -1)
+        if embedding.shape[1] != self.dim:
+            raise ValueError(f"Embedding has invalid dimension: {embedding.shape}. Expected dimension: {self.dim}.")
+        if self.metric == "cosine":
+            embedding = normalize(embedding)
+
         self.embeddings = np.append(self.embeddings, embedding.reshape(1, -1), axis=0)
         self.index_map.append(index)
 
-    def delete(self, index):
+    def delete(self, index: int) -> None:
         if index not in self.index_map:
-            raise ValueError(f"Index {index} not found")
+            raise ValueError(f"Index {index} not found.")
         row_to_delete = self.index_map.index(index)
         self.embeddings = np.delete(self.embeddings, row_to_delete, axis=0)
         del self.index_map[row_to_delete]
 
-    def save(self, filepath):
+    def save(self, filepath: str) -> None:
         np.savez_compressed(filepath, embeddings=self.embeddings, index_map=np.array(self.index_map))
 
-    def load(self, filepath):
+    def load(self, filepath: str) -> None:
         with np.load(filepath) as data:
             self.embeddings = data["embeddings"]
             self.index_map = data["index_map"].tolist()
 
-    def query(self, query_embedding, k=1):
+    def query(self, query_embedding: np.ndarray, k: int = 1) -> List[Tuple[int, float]]:
         if self.metric == "dot":
             similarities = np.dot(self.embeddings, query_embedding)
         elif self.metric == "cosine":
-            normalized_embeddings = self.embeddings / np.linalg.norm(self.embeddings, axis=1, keepdims=True)
-            normalized_query = query_embedding / np.linalg.norm(query_embedding)
-            similarities = np.dot(normalized_embeddings, normalized_query)
+            normalized_query = normalize(query_embedding)
+            similarities = np.dot(self.embeddings, normalized_query)
         else:
             raise ValueError(f"Invalid metric: {self.metric}. Supported metrics are 'dot' and 'cosine'.")
 
@@ -42,4 +54,4 @@ class Index:
         top_k_indices_sorted = top_k_indices[np.argsort(-similarities[top_k_indices])]
         top_k_values = similarities[top_k_indices_sorted]
 
-        return [(index, similarity) for index, similarity in zip(top_k_indices_sorted, top_k_values)]
+        return [(self.index_map[i], similarity) for i, similarity in zip(top_k_indices_sorted, top_k_values)]
--- tests/test_minivnn.py
+++ tests/test_minivnn.py
@@ -1,10 +1,18 @@
 import numpy as np
 import pytest
+
 from minivnn import Index
+from minivnn.minivnn import normalize
 
 
-def test_add():
-    index = Index(3)
+def test_normalize():
+    embedding = np.array([0.1, 0.2, 0.3])
+    normalized_embedding = normalize(embedding)
+    assert np.allclose(normalized_embedding, embedding / np.linalg.norm(embedding))
+
+
+def test_add_dot():
+    index = Index(3, metric="dot")
     embedding = np.array([0.1, 0.2, 0.3])
 
     index.add(1, embedding)
@@ -12,6 +20,20 @@ def test_add():
     assert len(index.index_map) == 1
     assert index.index_map[0] == 1
     assert np.allclose(index.embeddings[0], embedding)
+
+    with pytest.raises(ValueError):
+        index.add(1, embedding)
+
+
+def test_add_cosine():
+    index = Index(3, metric="cosine")
+    embedding = np.array([0.1, 0.2, 0.3])
+
+    index.add(1, embedding)
+    assert index.embeddings.shape == (1, 3)
+    assert len(index.index_map) == 1
+    assert index.index_map[0] == 1
+    assert np.allclose(index.embeddings[0], embedding / np.linalg.norm(embedding))
 
     with pytest.raises(ValueError):
         index.add(1, embedding)
@@ -59,14 +81,14 @@ def test_query():
     embedding2 = np.array([0.2, 0.3, 0.4])
     embedding3 = np.array([0.3, 0.4, 0.5])
 
-    index.add(1, embedding1)
-    index.add(2, embedding2)
-    index.add(3, embedding3)
+    index.add(2, embedding1)
+    index.add(3, embedding2)
+    index.add(4, embedding3)
 
     query_embedding = np.array([0.15, 0.25, 0.35])
 
     result = index.query(query_embedding, k=2)
 
     assert len(result) == 2
-    assert result[0][0] == 2
-    assert result[1][0] == 1
+    assert result[0][0] == 4
+    assert result[1][0] == 3
