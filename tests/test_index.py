1d2442aef824f9d7993d6a5d6dc2a2a14fceba3f
Adilzhan Ismailov <13088690+aismlv@users.noreply.github.com>
2023-05-10 23:51:33 +0100

    Validate inputs (#17)
    * Add query item
    
    * Use variables
    
    * Edit README
    
    * Fix typo
    
    * Validate inputs
    
    * Use fixtures
    
    * Bump version

--- tests/test_index.py
+++ tests/test_index.py
@@ -4,58 +4,72 @@ import pytest
 from minivan import Index
 
 
-def test_add_items_list():
-    index = Index(3, metric="dot_product")
-    embedding1 = np.array([0.1, 0.2, 0.3])
-    embedding2 = np.array([0.2, 0.3, 0.4])
+@pytest.fixture
+def embedding1():
+    return np.array([0.1, 0.2, 0.3])
 
-    index.add_items([1, 2], [embedding1, embedding2])
+
+@pytest.fixture
+def embedding2():
+    return np.array([0.2, 0.3, 0.4])
+
+
+@pytest.fixture
+def embedding3():
+    return np.array([0.3, 0.4, 0.5])
+
+
+@pytest.mark.parametrize(
+    "indices, embeddings",
+    [
+        ([1, 2], [np.array([0.1, 0.2, 0.3]), np.array([0.2, 0.3, 0.4])]),
+        ([1, 2], np.array([[0.1, 0.2, 0.3], [0.2, 0.3, 0.4]])),
+        (np.array([1, 2]), [np.array([0.1, 0.2, 0.3]), np.array([0.2, 0.3, 0.4])]),
+        (np.array([1, 2]), np.array([[0.1, 0.2, 0.3], [0.2, 0.3, 0.4]])),
+    ],
+)
+def test_add_items(indices, embeddings):
+    index = Index(3, metric="dot_product")
+    index.add_items(indices, embeddings)
+
     assert index.embeddings.shape == (2, 3)
     assert len(index.index_map) == 2
     assert index.index_map[0] == 1
-    assert np.allclose(index.embeddings[0], embedding1)
+    assert np.allclose(index.embeddings[0], np.array([0.1, 0.2, 0.3]))
+
+
+def test_invalid_add_items(embedding1, embedding2):
+    index = Index(3, metric="dot_product")
+    embeddings = np.vstack([embedding1, embedding2])
 
     with pytest.raises(KeyError):
         index.add_items([1], [embedding1])
+        index.add_items([1], [embedding1])
+    with pytest.raises(TypeError):
+        index.add_items("not indices", [embedding1])
     with pytest.raises(TypeError):
         index.add_items(["2"], [embedding1])
     with pytest.raises(ValueError):
         index.add_items([3, 4], [embedding1, np.array([0.1, 0.2])])
-
-
-def test_add_items_2d_array():
-    index = Index(3, metric="dot_product")
-    embeddings = np.array([[0.1, 0.2, 0.3], [0.2, 0.3, 0.4]])
-
-    index.add_items([1, 2], embeddings)
-    assert index.embeddings.shape == (2, 3)
-    assert len(index.index_map) == 2
-    assert index.index_map[0] == 1
-    assert np.allclose(index.embeddings[0], embeddings[0])
-
     with pytest.raises(ValueError):
         index.add_items([3], embeddings)
 
 
-def test_add_items_cosine():
+def test_add_items_cosine(embedding1):
     index = Index(3, metric="cosine")
-    embedding = np.array([0.1, 0.2, 0.3])
 
-    index.add_items([1], [embedding])
+    index.add_items([1], [embedding1])
     assert index.embeddings.shape == (1, 3)
     assert len(index.index_map) == 1
     assert index.index_map[0] == 1
-    assert np.allclose(index.embeddings[0], embedding / np.linalg.norm(embedding))
+    assert np.allclose(index.embeddings[0], embedding1 / np.linalg.norm(embedding1))
 
     with pytest.raises(KeyError):
-        index.add_items([1], [embedding])
+        index.add_items([1], [embedding1])
 
 
-def test_delete_items():
+def test_delete_items(embedding1, embedding2):
     index = Index(3)
-    embedding1 = np.array([0.1, 0.2, 0.3])
-    embedding2 = np.array([0.2, 0.3, 0.4])
-
     index.add_items([1, 2], [embedding1, embedding2])
 
     index.delete_items([1])
@@ -68,11 +82,8 @@ def test_delete_items():
         index.delete_items([1])
 
 
-def test_save_load(tmp_path):
+def test_save_load(tmp_path, embedding1, embedding2):
     index = Index(3, metric="dot_product")
-    embedding1 = np.array([0.1, 0.2, 0.3])
-    embedding2 = np.array([0.2, 0.3, 0.4])
-
     index.add_items([1, 2], [embedding1, embedding2])
 
     filepath = tmp_path / "index_data.npz"
@@ -97,11 +108,8 @@ def test_save_load(tmp_path):
         loaded_index.load(filepath)
 
 
-def test_load_from_file(tmp_path):
+def test_load_from_file(tmp_path, embedding1, embedding2):
     index = Index(3, metric="dot_product")
-    embedding1 = np.array([0.1, 0.2, 0.3])
-    embedding2 = np.array([0.2, 0.3, 0.4])
-
     index.add_items([1, 2], [embedding1, embedding2])
 
     filepath = tmp_path / "index_data.npz"
@@ -112,10 +120,7 @@ def test_load_from_file(tmp_path):
     assert loaded_index.index_map == index.index_map
 
 
-def test_query_dot_product():
-    embedding1 = np.array([0.1, 0.2, 0.3])
-    embedding2 = np.array([0.2, 0.3, 0.4])
-    embedding3 = np.array([0.3, 0.4, 0.5])
+def test_query_dot_product(embedding1, embedding2, embedding3):
     query_embedding = np.array([0.15, 0.25, 0.35])
 
     index = Index(3, metric="dot_product")
@@ -136,10 +141,7 @@ def test_query_dot_product():
         index.query(np.array([0.1, 0.2, 0.3, 0.4]), k=2)
 
 
-def test_query_cosine():
-    embedding1 = np.array([0.1, 0.2, 0.3])
-    embedding2 = np.array([0.2, 0.3, 0.4])
-    embedding3 = np.array([0.3, 0.4, 0.5])
+def test_query_cosine(embedding1, embedding2, embedding3):
     query_embedding = np.array([0.15, 0.25, 0.35])
 
     index = Index(3, metric="cosine")
@@ -151,11 +153,18 @@ def test_query_cosine():
     assert result[1][0] == 2
 
 
-def test_len():
-    index = Index(3)
-    embedding1 = np.array([0.1, 0.2, 0.3])
-    embedding2 = np.array([0.2, 0.3, 0.4])
+def test_query_item(embedding1, embedding2, embedding3):
+    index = Index(3, metric="dot_product")
+    index.add_items([2, 3, 4], [embedding1, embedding2, embedding3])
+    result = index.query_item(2, k=2)
 
+    assert len(result) == 2
+    assert result[0][0] == 4
+    assert 2 not in [item[0] for item in result]
+
+
+def test_len(embedding1, embedding2):
+    index = Index(3)
     assert len(index) == 0
 
     index.add_items([1, 2], [embedding1, embedding2])
@@ -165,11 +174,8 @@ def test_len():
     assert len(index) == 1
 
 
-def test_get_item():
+def test_get_item(embedding1, embedding2):
     index = Index(3)
-    embedding1 = np.array([0.1, 0.2, 0.3])
-    embedding2 = np.array([0.2, 0.3, 0.4])
-
     index.add_items([1, 2], [embedding1, embedding2])
     assert np.allclose(index.get_items([1]), embedding1)
     assert np.allclose(index.get_items([1, 2]), np.vstack([embedding1, embedding2]))
